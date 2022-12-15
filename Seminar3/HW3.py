e9535a94c3f9af756f9424b0e4fe0659f2564b60
AkhodeevaAnna <113910892+AkhodeevaAnna@users.noreply.github.com>
2022-12-15 13:44:50 +0300

    Дополнила HW3

--- Seminar3/HW3.py
+++ Seminar3/HW3.py
@@ -9,8 +9,6 @@
 # out
 # 1011
 number = int(input())
-while number < 0:
-    number = int(input("Введите число >= 0: "))
 def Ten_to_two (n):
     list = []
     if n == 0:
@@ -24,6 +22,14 @@ def Ten_to_two (n):
         while n > 0:
             list.insert(0, n % 2)
             n //= 2
+        while len(list) < 8:
+            list.insert(0,0)
+        for i in range (len(list)):
+            if list[i] == 0:
+                list[i] = 1
+            elif list[i] == 1:
+                list[i] = 0
+        list [len(list) - 1] = 1
     return list
 number_two = Ten_to_two(number)
 print(number_two)
