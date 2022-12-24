524a8f569481f45ead38f0fc3646e6139de9c2a9
AkhodeevaAnna <113910892+AkhodeevaAnna@users.noreply.github.com>
2022-12-24 03:29:20 +0300

    Seminar4

---
+++ Seminar4/Task4_2.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+def discr(a:int, b:int, c: int):
+    res = b**2-4*a*c
+    with open ('new_file.txt','a',encoding='utf-8') as F:
+        if res>0:
+            sqr1 = (-b+sqrt(res))/(2*a)
+            sqr2 = (-b-sqrt(res))/(2*a)
+            F.writelines([f'{sqr1}, {sqr2}\n'])
+        elif res ==0:
+            sqr1 = -b/(2*a)
+            F.write(f'{sqr1}\n')
+        else:
+            F.write('нет корней\n')
+
+for i in range(3):
+    a,b,c = int(input()), int(input()), int(input())
+    discr(a,b,c)
